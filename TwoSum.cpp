d9d3ce84e3849ae0d3f5e7e7eea8ce89d5bcdc5b
stormHan <stormHan1205@gmail.com>
2016-03-01 23:21:00 +0800

    leetcode

---
+++ TwoSum.cpp
@@ -0,0 +1,29 @@
+#include <vector>
+#include <map>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> twoSum(vector<int>& nums, int target) {
+		vector<int> index;
+		map<int, int> hmap;
+
+		for (int i = 0; i < nums.size(); i++)
+		{
+			if (!hmap.count(nums.at(i)))
+				hmap.insert(pair<int, int>(nums.at(i), i));
+			if (hmap.count(target - nums.at(i)))
+			{
+				int n = hmap[target - nums.at(i)];
+				if (n < i)
+				{
+					index.push_back(n);
+					index.push_back(i);
+				}
+
+			}
+
+		}
+		return index;
+	}
+};

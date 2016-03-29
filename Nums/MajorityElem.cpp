08aeb1e242aa3726ed0a60d996f68d2080e29b19
JWhan <stormhan1205@gmail.com>
2016-03-29 17:01:56 +0800

    Added files via upload

---
+++ Nums/MajorityElem.cpp
@@ -0,0 +1,35 @@
+﻿/*---------------leetcode-----------------
+----------------Majority Element----------
+----------------storm Han-----------------
+----------------2016.03.2----------------
+
+Question describing:
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+*/
+#include <vector>
+#include <map>
+
+using namespace std;
+
+int majorityElement(vector<int>& nums) {
+	int maxcount = 0;
+	map<int, int> hmap;
+
+	for (int i = 0; i < nums.size(); ++i)
+	{
+		if (!hmap.count(nums[i]))
+		{
+			hmap.insert(pair<int, int>(nums[i], 1));
+		}
+		else
+		{
+			hmap[nums[i]]++;
+		}
+
+		if (hmap[nums[i]] > maxcount) maxcount = hmap[nums[i]];
+		if (maxcount > nums.size() / 2) return nums[i];
+	}
+}

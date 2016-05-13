6721e181d9e4abacfa87dd9519d09fc8c5e2f920
stormHan <stormHan1205@gmail.com>
2016-05-13 22:12:09 +0800

    update my ProblemSet

---
+++ HappyNumber.cpp
@@ -0,0 +1,47 @@
+/*---------------leetcode-----------------
+---------------Level : Easy---------------
+---------------Happy Number---------------
+----------------storm Han-----------------
+----------------2016.05.13----------------
+
+Question describing:
+Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, 
+replace the number by the sum of the squares of its digits, and repeat the process until the number 
+equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+
+Those numbers for which this process ends in 1 are happy numbers.
+
+Example: 19 is a happy number
+
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+
+*/
+bool isHappy(int n)
+{
+	int sum = 0;
+	int count = 0;
+	do
+	{
+		while (n != 0)
+		{
+			int a = n % 10;
+			sum += a * a;
+			n = n / 10;
+		}
+
+		if (sum == 1) return true;
+		else
+		{
+			n = sum;
+			count++;
+			sum = 0;
+		}
+	} while (count < 100);
+
+	return false;
+}
